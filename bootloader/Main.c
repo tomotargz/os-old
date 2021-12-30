63c1ac9545b5484ec187b4428c5e659bbdfa8f60
tomotargz <nakamuratomota@gmail.com>
2021-12-30 22:00:22 +0900

    day04d: Load kernel to the correct address

--- bootloader/Main.c
+++ bootloader/Main.c
@@ -3,6 +3,7 @@
 
 // Uefi headers
 #include <Guid/FileInfo.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
@@ -14,6 +15,8 @@
 
 // Other headers
 #include "ProcessorBind.h"
+#include "Uefi/UefiMultiPhase.h"
+#include "elf.hpp"
 #include "frame_buffer_config.hpp"
 
 struct MemoryMap {
@@ -178,6 +181,28 @@ void Halt(void) {
     while (1) __asm__("hlt");
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr* ehdr, UINT64* first, UINT64* last) {
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    *first = MAX_UINT64;
+    *last = 0;
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+        if (phdr[i].p_type != PT_LOAD) continue;
+        *first = MIN(*first, phdr[i].p_vaddr);
+        *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+    }
+}
+
+void CopyLoadSegments(Elf64_Ehdr* ehdr) {
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+        if (phdr[i].p_type != PT_LOAD) continue;
+        UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+        CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
+        UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+        SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+    }
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE* system_table) {
     EFI_STATUS status;
@@ -261,26 +286,37 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
     EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
     UINTN kernel_file_size = file_info->FileSize;
 
-    // #@@range_begin(alloc_error)
-    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData,
-                                (kernel_file_size + 0xfff) / 0x1000,
-                                &kernel_base_addr);
+    VOID* kernel_buffer;
+    status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
     if (EFI_ERROR(status)) {
-        Print((CHAR16*)L"failed to allocate pages: %r", status);
+        Print((CHAR16*)L"failed to allocate pool: %r\n", status);
         Halt();
     }
-    // #@@range_end(alloc_error)
-    status = kernel_file->Read(kernel_file, &kernel_file_size,
-                               (VOID*)kernel_base_addr);
+    status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
     if (EFI_ERROR(status)) {
         Print((CHAR16*)L"error: %r", status);
         Halt();
     }
-    Print((CHAR16*)L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr,
-          kernel_file_size);
 
-    // #@@range_begin(exit_bs)
+    Elf64_Ehdr* kernel_ehdr = (Elf64_Ehdr*)kernel_buffer;
+    UINT64 kernel_first_addr, kernel_last_addr;
+    CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+    UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages,
+                                &kernel_first_addr);
+    if (EFI_ERROR(status)) {
+        Print((UINT16*)L"failed to allocate pages: %r\n", status);
+        Halt();
+    }
+    CopyLoadSegments(kernel_ehdr);
+    Print((UINT16*)L"Kernel: 0x%01x - 0x%01x\n", kernel_first_addr,
+          kernel_last_addr);
+    status = gBS->FreePool(kernel_buffer);
+    if (EFI_ERROR(status)) {
+        Print((UINT16*)L"failed to free pool: %r\n", status);
+        Halt();
+    }
+
     status = gBS->ExitBootServices(image_handle, memmap.map_key);
     if (EFI_ERROR(status)) {
         status = GetMemoryMap(&memmap);
@@ -294,9 +330,8 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
             Halt();
         }
     }
-    // #@@range_end(exit_bs)
 
-    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+    UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
 
     // #@@range_begin(pass_frame_buffer_config)
     struct FrameBufferConfig config = {(UINT8*)gop->Mode->FrameBufferBase,
