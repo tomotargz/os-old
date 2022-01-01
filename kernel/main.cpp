5fbdab049949f0c1c3bad52e9be416052b801134
tomotargz <nakamuratomota@gmail.com>
2022-01-01 15:54:26 +0900

    day05a: Draw 'A' on the screen

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,7 +1,26 @@
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 
 #include "frame_buffer_config.hpp"
+
+const uint8_t kFontA[16] = {
+    0b00000000,  //
+    0b00011000,  //    xx
+    0b00011000,  //    xx
+    0b00011000,  //    xx
+    0b00011000,  //    xx
+    0b00100100,  //   x  x
+    0b00100100,  //   x  x
+    0b00100100,  //   x  x
+    0b00100100,  //   x  x
+    0b01111110,  //  xxxxxx
+    0b01000010,  //  x    x
+    0b01000010,  //  x    x
+    0b01000010,  //  x    x
+    0b11100111,  // xxx  xxx
+    0b00000000,  //
+    0b00000000,  //
+};
 
 void* operator new(size_t size, void* buf) {
     return buf;
@@ -55,6 +74,18 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
     }
 };
 
+void WriteAscii(PixelWriter& writer, int x, int y, char c,
+                const PixelColor& color) {
+    if (c != 'A') return;
+    for (int dy = 0; dy < 16; ++dy) {
+        for (int dx = 0; dx < 8; ++dx) {
+            if (kFontA[dy] & (0b10000000 >> dx)) {
+                writer.Write(x + dx, y + dy, color);
+            }
+        }
+    }
+}
+
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
@@ -82,5 +113,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
             pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
         }
     }
+    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+    WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
     while (1) __asm__("hlt");
 }
