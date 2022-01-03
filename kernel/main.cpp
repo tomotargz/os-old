ee1e4d563f520d2013d91302d4da234da3d6f557
tomotargz <nakamuratomota@gmail.com>
2022-01-03 20:10:59 +0900

    day05f: Add printk

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -13,6 +13,18 @@ void operator delete(void* obj) noexcept {}
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  char s[1024];
+  va_start(ap, format);
+  int result = vsprintf(s, format, ap);
+  va_end(ap);
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -32,11 +44,12 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
-  char buf[128];
-  for (int i = 0; i < 60; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+
+  console =
+      new (console_buf) Console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
+
+  for (int i = 0; i < 30; ++i) {
+    printk("printk: %d\n", i);
   }
   while (1) __asm__("hlt");
 }
