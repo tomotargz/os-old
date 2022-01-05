bc4a47d30ea140f524412b7965d412a324dc8c57
tomotargz <nakamuratomota@gmail.com>
2022-01-05 17:21:28 +0900

    day06a: Add a mouse cursor

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -11,6 +11,38 @@ void* operator new(size_t size, void* buf) { return buf; }
 
 void operator delete(void* obj) noexcept {}
 
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+    "@              ",  //
+    "@@             ",  //
+    "@.@            ",  //
+    "@..@           ",  //
+    "@...@          ",  //
+    "@....@         ",  //
+    "@.....@        ",  //
+    "@......@       ",  //
+    "@.......@      ",  //
+    "@........@     ",  //
+    "@.........@    ",  //
+    "@..........@   ",  //
+    "@...........@  ",  //
+    "@............@ ",  //
+    "@......@@@@@@@@",  //
+    "@......@       ",  //
+    "@....@@.@      ",  //
+    "@...@ @.@      ",  //
+    "@..@   @.@     ",  //
+    "@.@    @.@     ",  //
+    "@@      @.@    ",  //
+    "@       @.@    ",  //
+    "         @.@   ",  //
+    "         @@@   ",  //
+};
+
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 char console_buf[sizeof(Console)];
@@ -38,18 +70,32 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       break;
   }
 
-  // Fill display with white
-  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-    for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      pixel_writer->Write(x, y, {255, 255, 255});
+  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+  const int kFrameHeight = frame_buffer_config.vertical_resolution;
+
+  FillRectangle(*pixel_writer, {0, 0}, {kFrameWidth, kFrameHeight - 50},
+                kDesktopBGColor);
+  FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth, 50},
+                {1, 8, 17});
+  FillRectangle(*pixel_writer, {0, kFrameHeight - 50},
+                {kFrameWidth / 5, kFrameHeight - 50}, {80, 80, 80});
+  FillRectangle(*pixel_writer, {10, kFrameHeight - 40}, {30, 30},
+                {160, 160, 160});
+
+  console = new (console_buf)
+      Console(*pixel_writer, kDesktopFGColor, kDesktopBGColor);
+  console->PutString("now, my os has a mouse cursor :D");
+
+  // draw a mouse cursor
+  for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+    for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+      if (mouse_cursor_shape[dy][dx] == '@') {
+        pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+      } else if (mouse_cursor_shape[dy][dx] == '.') {
+        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+      }
     }
   }
 
-  console =
-      new (console_buf) Console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
-
-  for (int i = 0; i < 30; ++i) {
-    printk("printk: %d\n", i);
-  }
   while (1) __asm__("hlt");
 }
